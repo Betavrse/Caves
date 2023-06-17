c50c7421a6aa2aef9cc42cb13a1b29691e7f6588
Betavrse <zehaoq.zq@gmail.com>
2023-06-17 02:56:37 +0100

    updated all models

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import * as THREE from 'three';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { TDSLoader } from 'three/addons/loaders/TDSLoader.js';
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { Scene, PerspectiveCamera } from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Sky } from 'three/addons/objects/Sky.js';
+import { DotScreenShader } from 'three/addons/shaders/DotScreenShader.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { BokehPass } from 'three/addons/postprocessing/BokehPass.js';
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
+import { GammaCorrectionShader } from 'three/addons/shaders/GammaCorrectionShader.js';
 
-let camera, scene, renderer, raycaster;
-let lightProbe;
+let camera, scene, renderer, raycaster, composer;
 let dirLight;
 var scrollY = 0;
 var PCobjHidden = false;
@@ -16,10 +24,13 @@ var SCobjHidden = true;
 let cameraPosition;
 let INTERSECTED;
 let theta = 0;
+let textureEquirec, textureCube;
+let sky, sun;
+const postprocessing = {};
+
+let sprite0, sprite1, sprite2, sprite3, sprite4;
 
 const pointer = new THREE.Vector2();
-const radius = 100;
-const frustumSize = 400;
 var touchStartY = 0;
 var _event = {
     y: 0,
@@ -29,6 +40,11 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+const API = {
+    lightProbeIntensity: 1.0,
+    directionalLightIntensity: 1,
+    envMapIntensity: 1
+};
 
 const newmaterial = new THREE.MeshPhysicalMaterial({
     reflectivity: 1,
@@ -56,57 +72,61 @@ renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true;
-renderer.physicallyCorrectLights = true;
-renderer.colorSpace = THREE.SRGBColorSpace;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 5;
+renderer.toneMappingExposure = 2;
+//renderer.physicallyCorrectLights = true;
+//renderer.colorSpace = THREE.SRGBColorSpace;
+//renderer.toneMapping = THREE.ACESFilmicToneMapping;
+//renderer.toneMappingExposure = 1;
 
 
 camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2000);
 //camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000 );
 camera.position.set(0, 100, 300);
+camera.focus = 50;
 //camera.LookAt(0,0,0);
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 
 controls.enableDamping = true;
 controls.screenSpacePanning = false;
 // an animation loop is required when either damping or auto-rotation are enabled
 controls.dampingFactor = 0.05;
-controls.minDistance = 200;
-controls.maxDistance = 500;
+controls.minDistance = 100;
+controls.maxDistance = 400;
 controls.maxPolarAngle = Math.PI / 2;
 controls.target.set(0, 0, 0);
 controls.enablePan = false;
 
 scene.background = new THREE.Color(0x000000);
-scene.fog = new THREE.Fog(0x444444, 200, 500);
+scene.fog = new THREE.Fog(0x000000, 500, 900);
 //scene.fog = new THREE.FogExp2( 0xcccccc, 1000 );
-const hemiLight = new THREE.HemisphereLight(0xa0a0a0, 0x444444, 1);
-hemiLight.position.set(0, 200, 0);
-scene.add(hemiLight);
+
 //
 
-lightProbe = new THREE.LightProbe();
-scene.add(lightProbe);
-scene.add(new THREE.AmbientLight(0x404040, 1));
-dirLight = new THREE.DirectionalLight(0xffffff, 0.3);
-dirLight.position.set(0, 500, 100);
-dirLight.castShadow = true;
-dirLight.shadow.camera.top = 180;
-dirLight.shadow.camera.bottom = - 100;
-dirLight.shadow.camera.left = - 120;
-dirLight.shadow.camera.right = 120;
-scene.add(dirLight);
 
+const lightProbe = new THREE.LightProbe();
+scene.add(lightProbe);
+scene.add(new THREE.AmbientLight(0xffffff, 1));
+const directionalLight = new THREE.DirectionalLight(0xffffff, API.directionalLightIntensity);
+directionalLight.position.set(0, 5000, 1000)
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.top = 180;
+directionalLight.shadow.camera.bottom = - 100;
+directionalLight.shadow.camera.left = - 120;
+directionalLight.shadow.camera.right = 120;
+const cloader = new THREE.CubeTextureLoader();
+cloader.setPath('textures/cube/Bridge2/');
+textureCube = cloader.load(['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg']);
+textureCube.colorSpace = THREE.SRGBColorSpace;
 
 var parent = new THREE.Group();
 scene.add(parent);
 
 var maxHeight = (container.clientHeight || container.offsetHeight) - window.innerHeight
-
+init();
+animate();
 function initThree() {
     renderer.setPixelRatio(window.devicePixelRatio || 1);
     //renderer.setClearColor(0x161216)
@@ -146,8 +166,8 @@ const loadingManager = new THREE.LoadingManager(() => {
     loadingScreen.addEventListener('transitionend', onTransitionEnd);
 
 });
-const size = 1000;
-const divisions = 20;
+const size = 2000;
+const divisions = 50;
 
 const gridHelper = new THREE.GridHelper(size, divisions);
 scene.add(gridHelper);
@@ -157,8 +177,12 @@ scene.add(gridHelper);
 ///////
 
 
-const loader = new TDSLoader(loadingManager);
-loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave00.3ds', function (object) {
+
+const loader = new GLTFLoader(loadingManager);
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( './jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
+loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave00.glb', function (gltf) {
     //object.position.z = -1200;
 
     const sprite = new THREE.TextureLoader().load('./src/textures/sprites/circle.png');
@@ -172,16 +196,23 @@ loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave00.3ds', 
     //const mergedGeo = BufferGeometryUtils.mergeBufferGeometries(geometries);
 
     //var mesh = new THREE.Points(mergedGeo, material);
-
-    object.scale.set(0.1, 0.1, 0.1);
+    const object = gltf.scene;
+    object.scale.set(8, 8, 8);
     // mesh.position.z = 0;
-    object.position.set(-100, 30, 100);
+    object.position.set(-120, 30, 100);
+    
+    object.traverse((o) => {
+        if (o.isMesh){
+            o.material.envMap = textureCube;
+        //o.material = newmaterial;
+    }
+      });
     //scene.add(mesh)
     scene.add(object);
     parent.add(object);
     //mesh.visible = true;
     object.visible = true;
-
+    
 
     /*
         document.getElementById("PCtoggle").addEventListener("click", function () {
@@ -197,142 +228,60 @@ loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave00.3ds', 
                 mesh.visible = false;
             }
     
-    
-        });
-        document.getElementById("WFtoggle").addEventListener("click", function () {
-            if (WFobjHidden) {
-                WFobjHidden = false;
-                // code to show object
-    
-                linesMesh.visible = true;
-            } else {
-                WFobjHidden = true;
-                // code to hide object
-    
-                linesMesh.visible = false;
-            }
-    
-    
-        });
-        document.getElementById("SCtoggle").addEventListener("click", function () {
-            if (SCobjHidden) {
-                SCobjHidden = false;
-                // code to show object
-    
-                object.visible = true;
-            } else {
-                SCobjHidden = true;
-                // code to hide object
-    
-                object.visible = false;
-            }
-    
-    
-        });*/
+    */
+}
+);
+loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave01.glb', function (gltf) {
+    const object = gltf.scene;
+    object.traverse((o) => {
+        if (o.isMesh){
+            o.material.envMap = textureCube;
+        //o.material = newmaterial;
+    }
+      });
+    object.scale.set(8, 8, 8);
+    object.position.set(-80, 30, -100);
+    object.rotation.set(0,-90,0);
+    scene.add(object);
+    parent.add(object);
+    object.visible = true;
 }
 );
 
-loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave01.3ds', function (object) {
 
-    const sprite = new THREE.TextureLoader().load('./src/textures/sprites/circle.png');
-    let material = new THREE.PointsMaterial({ size: 2, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: false });
-    /*
-        const geometries = [];
-        for (var i = 0; i < object.children.length; i++) {
-            geometries.push(object.children[i].geometry);
-        }
-    
-        const mergedGeo = BufferGeometryUtils.mergeBufferGeometries(geometries);
-    
-        var mesh = new THREE.Points(mergedGeo, material);
-    */
-    object.scale.set(0.1, 0.1, 0.1);
-    // mesh.position.z = 0;
-    object.position.set(-100, 30, -100);
-    //scene.add(mesh)
-    //scene.add(object);
-    parent.add(object);
-    //mesh.visible = false;
-    object.visible = true;
-
-});
-
-loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave02.3ds', function (object) {
-
-    const sprite = new THREE.TextureLoader().load('./src/textures/sprites/circle.png');
-    let material = new THREE.PointsMaterial({ size: 2, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: false });
-    /*
-        const geometries = [];
-        for (var i = 0; i < object.children.length; i++) {
-            geometries.push(object.children[i].geometry);
-        }
-    
-        const mergedGeo = BufferGeometryUtils.mergeBufferGeometries(geometries);
-    
-        var mesh = new THREE.Points(mergedGeo, material);
-    */
-    object.scale.set(0.1, 0.1, 0.1);
-    //mesh.position.z = 0;
+loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave03.glb', function (gltf) {
+    const object = gltf.scene;
+    object.traverse((o) => {
+        if (o.isMesh){
+            o.material.envMap = textureCube;
+        //o.material = newmaterial;
+    }
+      });
+    object.scale.set(8, 8, 8);
     object.position.set(100, 30, -100);
-    object.rotation.set(-120, -30, -90);
-    //scene.add(mesh)
-    //scene.add(object);
+    object.rotation.set(0,90,0);
+    scene.add(object);
     parent.add(object);
-    //mesh.visible = false;
     object.visible = true;
+}
+);
 
-});
-
-loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave04.3ds', function (object) {
-
-    const sprite = new THREE.TextureLoader().load('./src/textures/sprites/circle.png');
-    let material = new THREE.PointsMaterial({ size: 2, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: false });
-    /*
-        const geometries = [];
-        for (var i = 0; i < object.children.length; i++) {
-            geometries.push(object.children[i].geometry);
-        }
-    
-        const mergedGeo = BufferGeometryUtils.mergeBufferGeometries(geometries);
-    
-        var mesh = new THREE.Points(mergedGeo, material);
-    */
-    object.scale.set(0.1, 0.1, 0.1);
-    // mesh.position.z = 0;
-    object.position.set(100, 30, 100);
-    //scene.add(mesh)
-    //scene.add(object);
+loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave04.glb', function (gltf) {
+    const object = gltf.scene;
+    object.traverse((o) => {
+        if (o.isMesh){
+            o.material.envMap = textureCube;
+        //o.material = newmaterial;
+    }
+      });
+    object.scale.set(8, 8, 8);
+    object.position.set(80, 30, 100);
+    object.rotation.set(0,45,0);
+    scene.add(object);
     parent.add(object);
-    // mesh.visible = false;
     object.visible = true;
-
-});
-
-loader.load('https://storage.googleapis.com/fluidplate.com/Buracos/cave05.3ds', function (object) {
-
-    const sprite = new THREE.TextureLoader().load('./src/textures/sprites/circle.png');
-    let material = new THREE.PointsMaterial({ size: 2, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: false });
-    /*
-        const geometries = [];
-        for (var i = 0; i < object.children.length; i++) {
-            geometries.push(object.children[i].geometry);
-        }
-    
-        const mergedGeo = BufferGeometryUtils.mergeBufferGeometries(geometries);
-    
-        var mesh = new THREE.Points(mergedGeo, material);
-    */
-    object.scale.set(0.1, 0.1, 0.1);
-    // mesh.position.z = 0;
-    object.position.set(100, 30, 100);
-    //scene.add(mesh)
-    //scene.add(object);
-    parent.add(object);
-    // mesh.visible = false;
-    object.visible = true;
-
-});
-
+}
+);
 
 
 /////////
@@ -427,29 +376,29 @@ textloader.load('./src/fonts/helvetiker_regular.typeface.json', function (font) 
  * Particles
  */
 // Geometry
-
-const particlesCount = 60000
+/*
+const particlesCount = 5000
 const positions = new Float32Array(particlesCount * 3)
 
 
 for(let i = 0; i < particlesCount; i++)
 {
-    positions[i * 3 + 0] = (Math.random() - 0.5) * 5000
-    positions[i * 3 + 1] = (Math.random() - 0.5) * 5000
-    positions[i * 3 + 2] = (Math.random() - 0.5) * 5000
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 1000
+    positions[i * 3 + 1] = (Math.random() - 0.5) * 1000
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 1000
 }
 const particlesGeometry = new THREE.BufferGeometry()
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-    color: '#ffeded',
-    sizeAttenuation: true,
-    size: 0.5
+    color: '#878787',
+    sizeAttenuation: false,
+    size: 1
 })
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
-
+*/
 function lerp(a, b, t) {
     return ((1 - t) * a + t * b);
 }
@@ -460,7 +409,27 @@ function init() {
     //divContainer.addEventListener('wheel', onWheel, { passive: false });
     //divContainer.addEventListener('touchstart', onTouchStart, { passive: false });
     //divContainer.addEventListener('touchmove', onTouchMove, { passive: false });
-    animate()
+    
+    //initSky();
+    initPostprocessing();
+    renderer.autoClear = false;
+    const effectController = {
+
+        focus: 50.0,
+        aperture: 10,
+        maxblur: 0.01
+
+    };
+
+    const matChanger = function ( ) {
+
+        postprocessing.bokeh.uniforms[ 'focus' ].value = effectController.focus;
+        postprocessing.bokeh.uniforms[ 'aperture' ].value = effectController.aperture * 0.00001;
+        postprocessing.bokeh.uniforms[ 'maxblur' ].value = effectController.maxblur;
+
+    };
+
+    matChanger();
 }
 
 window.addEventListener('resize', () => {
@@ -475,44 +444,25 @@ window.addEventListener('resize', () => {
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    postprocessing.composer.setSize( sizes.width, sizes.height );
 })
 
 function animate() {
     // render the 3D scene
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate,renderer.domElement);
     controls.update();
     // relaunch the 'timer' 
     render();
-
 }
 
 function render() {
     //var dtime = Date.now() - startTime;
     var time = Date.now() * 0.0005;
     parent.rotation.y += 0.0004;
-    raycaster.setFromCamera(pointer, camera);
-    const intersects = raycaster.intersectObjects(scene.children, false);
-    if (intersects.length > 0) {
+    //raycaster.setFromCamera(pointer, camera);
 
-        if (INTERSECTED != intersects[0].object) {
-
-            if (INTERSECTED) INTERSECTED.material;
-
-            INTERSECTED = intersects[0].object;
-
-            //INTERSECTED.material = newmaterial;
-
-        }
-
-    } else {
-
-        if (INTERSECTED) INTERSECTED.material;
-
-        INTERSECTED = null;
-
-    }
-
-    renderer.render(scene, camera);
+    //renderer.render(scene, camera);
+    postprocessing.composer.render( 0.1 );
 
 }
 
@@ -566,11 +516,34 @@ function onTouchMove(e) {
     scroll(e)
 };
 
-init();
+
 
 
 function onTransitionEnd(event) {
 
     event.target.remove();
+
+}
+
+function initPostprocessing() {
+
+    const renderPass = new RenderPass( scene, camera );
+
+    const bokehPass = new BokehPass( scene, camera, {
+        focus: 200,
+        aperture: 10,
+        maxblur: 0.01
+    } );
+
+    const outputPass = new ShaderPass( GammaCorrectionShader );
+
+    const composer = new EffectComposer( renderer );
+
+    composer.addPass( renderPass );
+    composer.addPass( bokehPass );
+    composer.addPass( outputPass );
+
+    postprocessing.composer = composer;
+    postprocessing.bokeh = bokehPass;
 
 }
